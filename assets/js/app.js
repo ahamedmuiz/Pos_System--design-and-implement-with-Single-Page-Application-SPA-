85f8d8b96a41a84662274079d7fe68a5936663ee
ahamedmuiz119@gmail.com <ahamedmuiz119@gmail.com>
2025-11-08 13:46:33 +0530

    init commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,32 @@
-$(document).ready(()=>{
-$("#home-page").show
+import { loadAllCustomers, addCustomer, updateCustomer, deleteCustomer } from "../../controller/CustomerController.js";
 
-$(".nav-link").click((e) =>{
-    e.preventDefault()
+$(document).ready(function () {
+  // SPA Navigation
+  $(".nav-link").click(function () {
+    const target = $(this).data("target");
+    $(".page-section").hide();
+    $("#" + target).show();
+    $(".nav-link").removeClass("active");
+    $(this).addClass("active");
+  });
 
-    $(".page").hide()
+  // Load initial data
+  loadAllCustomers();
 
-    const target = $(this).data("target")
-    console.log(target)
-})
-})
+  // Customer Events
+  $("#btnAddCustomer").click(addCustomer);
+  $("#btnUpdateCustomer").click(updateCustomer);
+  $("#btnDeleteCustomer").click(deleteCustomer);
+
+  // Table click → fill form
+  $("#customerTable").on("click", "tr", function () {
+    let data = $(this).children("td").map(function () {
+      return $(this).text();
+    }).get();
+
+    $("#customerId").val(data[0]);
+    $("#customerName").val(data[1]);
+    $("#customerAddress").val(data[2]);
+    $("#customerSalary").val(data[3]);
+  });
+});
