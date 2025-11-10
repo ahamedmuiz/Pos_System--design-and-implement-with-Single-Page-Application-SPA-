a6237cb7b0e405b65babfa3278279074c223911c
ahamedmuiz119@gmail.com <ahamedmuiz119@gmail.com>
2025-11-10 10:30:00 +0530

    init commit

--- db/DB.js
+++ db/DB.js
@@ -3,5 +3,10 @@ export const customer_array = [
   { id: "C002", name: "Jane Smith", address: "Kandy", salary: 45000 },
 ];
 
-export const item_array = [];
+export const item_array = [
+  { code: "I001", name: "Laptop", price: 120000, qty: 10 },
+  { code: "I002", name: "Mouse", price: 2500, qty: 30 },
+  { code: "I003", name: "Keyboard", price: 4500, qty: 15 },
+];
+
 export const order_array = [];
