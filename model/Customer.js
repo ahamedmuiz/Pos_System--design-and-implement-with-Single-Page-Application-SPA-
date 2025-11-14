687008e85cbcfb346306e73edee1f35e613fe580
ahamedmuiz119@gmail.com <ahamedmuiz119@gmail.com>
2025-11-14 22:15:13 +0530

    fixed modeland controller

--- model/Customer.js
+++ model/Customer.js
@@ -1,20 +1,42 @@
 export default class Customer {
+
   constructor(id, name, address, salary) {
+    
     this._id = id;
     this._name = name;
     this._address = address;
     this._salary = salary;
   }
 
-  get id() { return this._id; }
-  set id(id) { this._id = id; }
+  get id() { 
+    return this._id; 
+  }
 
-  get name() { return this._name; }
-  set name(name) { this._name = name; }
+  set id(id) {
+     this._id = id;
+     }
 
-  get address() { return this._address; }
-  set address(address) { this._address = address; }
+  get name() { 
+    return this._name; 
+  }
 
-  get salary() { return this._salary; }
-  set salary(salary) { this._salary = salary; }
+  set name(name) { 
+    this._name = name;
+   }
+
+  get address() { 
+    return this._address;
+   }
+
+  set address(address) { 
+    this._address = address;
+   }
+
+  get salary() { 
+    return this._salary;
+   }
+
+  set salary(salary) {
+     this._salary = salary;
+     }
 }
